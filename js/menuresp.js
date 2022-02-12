b708b6b0d0f13f42db3eb008691f580212f1af1f
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2022-02-12 14:27:44 -0300

    Commit inicial
    Commit do site institucional  procempre 2022.1 eject.

---
+++ js/menuresp.js
@@ -0,0 +1,10 @@
+const botaomobile = document.getElementById('botaomobile');
+
+
+function toggleMenu(){
+    const botaomobile = document.getElementById('nav');
+    nav.classList.toggle('active')
+   
+}
+
+botaomobile.addEventListener('click', toggleMenu);
